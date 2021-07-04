76785a8f94addc0fda62db616f340b1d6d97762f
kldaji@naver.com <kldaji@naver.com>
2021-07-04 12:22:44 +0900

    21-07-04

--- BJ/String/1254-팰린드롬만들기.py
+++ BJ/String/1254-팰린드롬만들기.py
@@ -1,25 +1,33 @@
 """
 <String>
+<Brute Force>
 """
-
-"""
-<오답>
-"""
-
 from sys import *
 
+# string
 S = stdin.readline().rstrip()
 
+"""
+S[len(S) - 1 : i - 1 : -1] -> 0 일때 공백만 출력되서
+S == S[::-1] 조건을 따로 두었다.
 
-# s1 = S
-# s2 = S
+하지만 이를 해결하기 위한 방법 : 
+S[i:][::-1]을 사용하면 된다.
+"""
+if S == S[::-1]:
+    print(len(S))
+else:
+    # find palindrome
+    for i in range(1, len(S)):
+        if S[i:] == S[len(S) - 1 : i - 1 : -1]:
+            S += S[i - 1 :: -1]
+            break
 
-# index = len(s1) - 2
-# while s1 != s1[::-1]:
-#     s1 += s1[index]
-#     index -= 1
+    print(len(S))
 
-# index = 0
-# while s2 != s2[::1]:
-#     s2 += s2[index]
-#     index -= 1
+
+# for i in range(len(S)):
+#     print(S[i:][::-1])
+#     if S[i:] == S[i:][::-1]:
+#         print(len(S) + i)
+#         break
