6743fe067215515d7aa0bb27923ddad36cebcbc0
kldaji@naver.com <kldaji@naver.com>
2021-08-16 13:34:34 +0900

    2741번 N 찍기 - complete

---
+++ intellij/kotlin/baekjoon/src/main/kotlin/step_by_step/step3/2741.kt
@@ -0,0 +1,12 @@
+// TODO: N 찍기
+package step_by_step.step3
+
+import java.util.*
+
+fun main() {
+    val scan = Scanner(System.`in`)
+    val n = scan.nextInt()
+    for(i in 1..n) {
+        println(i)
+    }
+}
