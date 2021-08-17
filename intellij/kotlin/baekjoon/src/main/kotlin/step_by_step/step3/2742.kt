4eb7099ee6dc4130e554c323b5e34ffed2034c07
kldaji@naver.com <kldaji@naver.com>
2021-08-17 13:20:43 +0900

    2742번 기찍 N - complete

---
+++ intellij/kotlin/baekjoon/src/main/kotlin/step_by_step/step3/2742.kt
@@ -0,0 +1,12 @@
+// TODO: 기찍 N
+package step_by_step.step3
+
+import java.util.*
+
+fun main() {
+    val scan = Scanner(System.`in`)
+    val n = scan.nextInt()
+    for(i in n downTo 1) {
+        println(i)
+    }
+}
