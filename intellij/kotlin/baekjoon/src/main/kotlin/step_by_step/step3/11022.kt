d4568204e37617fb9f7cc2312645aee859919c0f
kldaji@naver.com <kldaji@naver.com>
2021-08-17 16:52:01 +0900

    11022번 A+B - 8 - complete

---
+++ intellij/kotlin/baekjoon/src/main/kotlin/step_by_step/step3/11022.kt
@@ -0,0 +1,14 @@
+// TODO: A+B - 8
+package step_by_step.step3
+
+import java.util.*
+
+fun main() {
+    val scan = Scanner(System.`in`)
+    val t = scan.nextInt()
+    repeat(t) {
+        val a = scan.nextInt()
+        val b = scan.nextInt()
+        println("Case #${it+1}: $a + $b = ${a+b}")
+    }
+}
