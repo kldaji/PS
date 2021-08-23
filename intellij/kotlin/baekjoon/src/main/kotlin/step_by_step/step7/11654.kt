840114d8922ae600e6a91348268d8ac7329a24b5
kldaji@naver.com <kldaji@naver.com>
2021-08-23 09:33:01 +0900

    11654번 아스키 코드 - complete

---
+++ intellij/kotlin/baekjoon/src/main/kotlin/step_by_step/step7/11654.kt
@@ -0,0 +1,9 @@
+// TODO: 아스키 코드
+package step_by_step.step7
+
+fun main() {
+    val c = readLine().toString()
+    println(c.first().code)
+}
+
+// https://stackoverflow.com/questions/47234812/how-to-convert-char-to-ascii-value-in-kotlin-language
