8df51f0841b50430be77a41f02a8722bbbeb9a0c
kldaji@naver.com <kldaji@naver.com>
2021-09-11 08:59:44 +0900

    [ADD] change list

--- intellij/kotlin/baekjoon/src/main/kotlin/step_by_step/step3/test.kt
+++ intellij/kotlin/baekjoon/src/main/kotlin/step_by_step/step3/test.kt
@@ -1,15 +1,7 @@
 package step_by_step.step3
-// 랜덤 텍스트 생성
-private fun spawnRandomText(): String {
-    val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
-    var randomString = ""
-    val num = kotlin.random.Random.nextInt(2, 6)
-    randomString = (1..num)
-        .map { i -> kotlin.random.Random.nextInt(0, charPool.size) }
-        .map(charPool::get)
-        .joinToString("")
-    return randomString
-}
+
 fun main() {
-    println(spawnRandomText())
+    val a = "FF"
+    val b = a.toLong(16)
+    println(b)
 }
