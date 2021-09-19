21fd59e7de95f9be6739e1030660c02aee0d7da2
kldaji <kldaji@naver.com>
2021-09-19 17:04:38 +0900

    [3053번] 택시 기하학 - complete

---
+++ intellij/kotlin/baekjoon/src/main/kotlin/step_by_step/step9/3053.kt
@@ -0,0 +1,8 @@
+// TODO: 택시 기하학
+package step_by_step.step9
+
+fun main() {
+    val r = readLine()!!.toDouble()
+    println(String.format("%.6f", r * r * Math.PI))
+    println(String.format("%.6f", r*r*2f))
+}
